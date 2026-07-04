b16c1f8b992285a4ade40802225fe027ae10db33
LI TE <LZYF14@GMAIL.COM>
2026-07-04 19:01:28 +0800

    0704_

--- gnn_heatmap/config_paths.py
+++ gnn_heatmap/config_paths.py
@@ -13,7 +13,7 @@ DIR_2D_OUTPUT = ROOT / "output_2d"
 DEFAULT_OCCUPIED_SPACE_INPUT = DIR_OBS_INPUT / "occupied_space.json"
 DEFAULT_STREET_ELEMENTS_CSV = ROOT / "street elements.csv"
 
-DEFAULT_3D_HEATMAP_JSON = DIR_3D_OUTPUT / "occupied_space.heatmap.json"
+DEFAULT_3D_HEATMAP_JSON = DIR_OBS_INPUT / "occupied_space.heatmap.json"
 
 DEFAULT_GROUND_TOP_MASK_SOURCE = DIR_OBS_INPUT / "ground_top_observations_live.json"
 
